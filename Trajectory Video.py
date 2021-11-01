b5df27fbfe6d0a1cba5c3c0943c48ef32df0b638
Inhyeok Baek <bih011122@gmail.com>
2021-11-01 18:16:32 +0900

    Update Trajectory Video.py

--- Trajectory Video.py
+++ Trajectory Video.py
@@ -7,6 +7,8 @@ Original file is located at
     https://colab.research.google.com/drive/1aJ_MR1fhBmZN1C5v4dHHzmcOQReSo7Z2
 """
 
+"""Sample code for Video S5-6"""
+
 import numpy as np
 import pandas as pd
 import cv2
